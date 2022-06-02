e3531f9ce9bdb7fcaf098970e65af35e4e0810cc
mehulo07 <mehulmakwana339@gmail.com>
2022-06-02 13:50:08 +0530

    Print all substing of given string

---
+++ src/main/java/com/mehul/example/recursion/FindAllSubsetOfGivenString.java
@@ -0,0 +1,20 @@
+package com.mehul.example.recursion;
+
+public class FindAllSubsetOfGivenString {
+
+	static void printSub(String str, String curr, int index) {
+		if (index == str.length()) {
+			System.out.print(curr + " ");
+			return;
+		}
+
+		printSub(str, curr, index + 1);
+		printSub(str, curr + str.charAt(index), index + 1);
+	}
+
+	public static void main(String[] args) {
+		String str = "MEHUl";
+		printSub(str, "", 0);
+
+	}
+}
